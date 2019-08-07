33bebb29dcde955960ab80fdb07101c2eeb16d1a
hassan elhamaky <hassan.elhamaky@yahoo.com>
2019-08-07 23:51:33 +0200

    service modified to use SNAPSHOTCHANGES insted of VALUECHANGES so we can get the id of each element which was not possible before that modification

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 import { Item } from '../models/item';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 
@@ -11,7 +12,14 @@ export class ItemService {
 
 
   constructor(public afs: AngularFirestore) {
-    this.items = this.afs.collection('items').valueChanges();
+    //this.items = this.afs.collection('items').valueChanges();
+    this.items = this.afs.collection('items').snapshotChanges().pipe(map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    }));
    }
 
    getItems(){
